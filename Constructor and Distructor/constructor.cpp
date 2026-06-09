9519e044c4b0f64223393b4fbbabe97a0917c9aa
Nitin Kumar <nitinkumar86190@gmail.com>
2026-06-09 14:42:17 +0530

    constructor and distructor

---
+++ Constructor and Distructor/constructor.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+class A {
+    string name;
+    int age;
+public:
+    A() {
+        cout << "Constructor called" << endl;
+    }
+    A(string name,int age){
+        this->name=name;
+        this->age=age;
+    }
+    void display(){
+        cout<<"Name "<<name<<endl;
+        cout<<"Age "<<age;
+    }
+
+   
+};
+
+int main() {
+    A obj1;
+    A obj2("nitin",20);
+    obj2.display();
+    return 0;
+}
---
+++ sizeofclass.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+class A {
+    char b;
+    char c;
+    int a;
+};
+int main() {
+    cout << "Size of class A is " << sizeof(A) << " bytes" << endl;
+    return 0;
+}
